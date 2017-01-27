35af328b1642ab30a1f7911942a2a4c0f40f4a6d
jkoller90 <jmk4411@gmail.com>
2017-01-27 15:20:40 -0500

    can now open image in new window...

--- script.js
+++ script.js
@@ -29,8 +29,27 @@ $(document).on('ready', function () {
   ]
 	});
 });
-
-$("button").click(function(event){
+var canvas = document.getElementById("invisiblecanvas");
+context = canvas.getContext('2d');
+var base_image = new Image(1440, 2560);
+var image_fromDOM;
+$("button").click(function (event) {
 	event.preventDefault();
-	alert("Will you download this");
+	var x = confirm("Will you download this?");
+	if (x) {
+		var dom_search = $(".slick-track").children();
+		dom_search.each(function (i) {
+				//	console.log($(x[i]).attr("aria-hidden") !== "true");
+				if ($(dom_search[i]).attr("aria-hidden") !== "true") {
+//					console.log($(dom_search[i])[0].children[0]); 
+					image_fromDOM = $($(dom_search[i])[0].children[0]).attr("src"); 
+					window.open(image_fromDOM);
+//					base_image.src = image_fromDOM; 
+//					context.drawImage(base_image, 0, 0); 
+//					canvas.toBlob(function (blob) {
+//						saveAs(blob, "screenshot.png");
+//					}, "image/png");
+				}
+	});
+}
 });
