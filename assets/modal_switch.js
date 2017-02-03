738d90856de1aab9b8c6c61c53b1fcaf5337f8e9
jkoller90 <jmk4411@gmail.com>
2017-02-02 22:06:56 -0500

    adjustments made

--- assets/modal_switch.js
+++ assets/modal_switch.js
@@ -6,24 +6,41 @@ $(".DLimg").click(function (event) {
 	$("#gameModal").css("visibility", "hidden");
 	$("main.modal-overlay").attr("style", "visibility:show; padding-top: 73px; padding-bottom: 54px; background-image:url(" + wallpaper + "); background-size: 100% 100%");
 });
-
-//var image_fromDOM;
-$("#wallpaperModal").on("taphold", function() {
-	//to download this? 
-//	console.log('hi');
-	var toDownload = confirm("Download this wallpaper?");
-	if (toDownload) {
+var toDownload = false;
+$("#wallpaperModal").click(function () {
+	toDownload = confirm("Would you like to download this image?");
+	if(toDownload){
 		window.open(wallpaper);
+	}
+	//to download this? 
+	//	console.log('hi');	
+	//logic for adding point to user's score 
+	//not connected to a working db, but basic logic is here as an outline of how it could work
+	$.ajax({
+		type: "post"
+		, url: "userPTupdate.php"
+		, data: "pointIncrement=" + 1
+		, success: function (data) {
+			console.log(data);
 		}
-
-		//logic for adding point to user's score 
-		//not connected to a working db, but basic logic is here as an outline of how it could work
-		$.ajax({
-			type: "post"
-			, url: "userPTupdate.php"
-			, data: "pointIncrement=" + 1
-			, success: function (data) {
-				console.log(data);
-			}
-		});
 	});
+});
+
+$("#wallpaperModal").on("taphold", function () {
+		toDownload = confirm("Would you like to download this image?");
+	if(toDownload){
+		window.open(wallpaper);
+	}
+	//to download this? 
+	//	console.log('hi');	
+	//logic for adding point to user's score 
+	//not connected to a working db, but basic logic is here as an outline of how it could work
+	$.ajax({
+		type: "post"
+		, url: "userPTupdate.php"
+		, data: "pointIncrement=" + 1
+		, success: function (data) {
+			console.log(data);
+		}
+	});
+});
