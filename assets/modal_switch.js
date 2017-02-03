6051d47b4cc5e3d36a85fbcbc5dcd745ec41dbc7
jkoller90 <jmk4411@gmail.com>
2017-02-02 22:21:24 -0500

    more fixes

--- assets/modal_switch.js
+++ assets/modal_switch.js
@@ -4,19 +4,17 @@ $(".DLimg").click(function (event) {
 	//	$("#downloadBanner").find("img").attr("src", wallpaper);
 	//	$("#wallpaperModal").find("img").attr("src", wallpaper);
 	$("#gameModal").css("visibility", "hidden");
-	$("main.modal-overlay").attr("style", "visibility:show; padding-top: 73px; padding-bottom: 54px; background-image:url(" + wallpaper + "); background-size: 100% 100%");
+	
+//	$("main.modal-overlay").attr("style", "visibility:show; padding-top: 73px; padding-bottom: 54px; background-image:url(" + wallpaper + "); background-size: 100% 100%");
+	$("#wallpaperModal").attr("style", "visibility:show; padding-top: 73px; padding-bottom: 54px;");
+	
+	$("#tobedownloaded").attr("src", wallpaper);
 });
 var toDownload = false;
 $("#wallpaperModal").click(function () {
 	toDownload = confirm("Would you like to download this image?");
 	if(toDownload){
-		window.open(wallpaper);
-	}
-	//to download this? 
-	//	console.log('hi');	
-	//logic for adding point to user's score 
-	//not connected to a working db, but basic logic is here as an outline of how it could work
-	$.ajax({
+			$.ajax({
 		type: "post"
 		, url: "userPTupdate.php"
 		, data: "pointIncrement=" + 1
@@ -24,17 +22,17 @@ $("#wallpaperModal").click(function () {
 			console.log(data);
 		}
 	});
+	}
+	//to download this? 
+	//	console.log('hi');	
+	//logic for adding point to user's score 
+	//not connected to a working db, but basic logic is here as an outline of how it could work
+
 });
 
 $("#wallpaperModal").on("taphold", function () {
 		toDownload = confirm("Would you like to download this image?");
 	if(toDownload){
-		window.open(wallpaper);
-	}
-	//to download this? 
-	//	console.log('hi');	
-	//logic for adding point to user's score 
-	//not connected to a working db, but basic logic is here as an outline of how it could work
 	$.ajax({
 		type: "post"
 		, url: "userPTupdate.php"
@@ -43,4 +41,10 @@ $("#wallpaperModal").on("taphold", function () {
 			console.log(data);
 		}
 	});
+	
+	}
+	//to download this? 
+	//	console.log('hi');	
+	//logic for adding point to user's score 
+	//not connected to a working db, but basic logic is here as an outline of how it could work
 });
