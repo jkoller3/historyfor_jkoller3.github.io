32eb544a15f96b63166d810a92ce98547678f598
jkoller90 <jmk4411@gmail.com>
2017-02-02 22:30:36 -0500

    second modal complete

--- assets/modal_switch.js
+++ assets/modal_switch.js
@@ -1,19 +1,21 @@
 var wallpaper;
 $(".DLimg").click(function (event) {
 	wallpaper = $(this).parent().find("img").attr("src");
-	//	$("#downloadBanner").find("img").attr("src", wallpaper);
-	//	$("#wallpaperModal").find("img").attr("src", wallpaper);
 	$("#gameModal").css("visibility", "hidden");
-	
-//	$("main.modal-overlay").attr("style", "visibility:show; padding-top: 73px; padding-bottom: 54px; background-image:url(" + wallpaper + "); background-size: 100% 100%");
 	$("#wallpaperModal").attr("style", "visibility:show; padding-top: 73px; padding-bottom: 54px;");
 	
 	$("#tobedownloaded").attr("src", wallpaper);
 });
+
+
+//on load, the z-index of the image will be below the overlay. the confirm dialog will bring the image up to be available to a contextmenu in a taphold event 
 var toDownload = false;
 $("#wallpaperModal").click(function () {
 	toDownload = confirm("Would you like to download this image?");
 	if(toDownload){
+		alert("Please tap and hold to save the wallpaper.");
+		var imageZshift = 'position:fixed;top:0px;left:0px;width:100%;height:100%;z-index:1;'
+		$("#tobedownloaded").attr("style", imageZshift);
 			$.ajax({
 		type: "post"
 		, url: "userPTupdate.php"
