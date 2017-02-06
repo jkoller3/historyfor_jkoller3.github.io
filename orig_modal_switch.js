1263b1d7902cc9721cd2f7b83b07239a24d82aa3
jkoller90 <jmk4411@gmail.com>
2017-02-06 13:11:16 -0500

    making branch to preserve comment on line 70-74

--- orig_modal_switch.js
+++ orig_modal_switch.js
@@ -1,22 +1,25 @@
-var incrementScoreVar = 0;
 var wallpaper;
 $(".DLimg").click(function (event) {
 	wallpaper = $(this).parent().find("img").attr("src");
-	$("#gameModal").css("visibility", "hidden"); //hide modal to show new modal
+	$("#gameModal").css("visibility", "hidden");
 	$("#wallpaperModal").attr("style", "visibility:show; padding-top: 73px; padding-bottom: 54px;");
+	
 	$("#tobedownloaded").attr("src", wallpaper);
-	incrementScoreVar++; //<- just for readibility.
-	alert("Tap and hold on the image to download the wallpaper!");
 });
 
 
 //on load, the z-index of the image will be below the overlay. the confirm dialog will bring the image up to be available to a contextmenu in a taphold event 
-
+var toDownload = false;
+var dialogStop = false;
 $("#tobedownloaded").click(function (e) {
+	if(dialogStop === false){
+		toDownload = confirm("Would you like to download this image?");
+		if(toDownload){
+			alert("Please tap and hold to save the wallpaper.");
+			dialogStop = true;
 			var imageZshift = 'position:fixed;top:0px;left:0px;width:100%;height:100%;z-index:1;';
 			$("#tobedownloaded").attr("style", imageZshift);
-			$("#closeIMG").attr("style", "z-index:1;");
-
+			$("#closeIMG").attr("style", "z-index:2;")
 //				$.ajax({
 //			type: "post"
 //			, url: "userPTupdate.php"
@@ -25,8 +28,9 @@ $("#tobedownloaded").click(function (e) {
 //				console.log(data);
 //			}
 //		});
-//		}
+		}
 		
+	}
 });
 
 
@@ -36,6 +40,9 @@ $(".modal-close").click(function(){
 
 var carouselpage; 
 $("#closeIMG").click(function(){
+
 	$("#wallpaperModal").css("visibility", "hidden");
 	$("#gameModal").attr("style", "visibility:show; padding-top: 73px; padding-bottom: 54px;");
+	
+//	$("#tobedownloaded").attr("src", wallpaper);
 });
